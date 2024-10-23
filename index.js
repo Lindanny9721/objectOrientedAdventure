2f66f41a5380f47bf9edad946e1313e70180abfb
Lindanny9721 <32419597+Lindanny9721@users.noreply.github.com>
2024-10-23 15:02:14 -0400

    Finish part 2

--- index.js
+++ index.js
@@ -17,3 +17,18 @@ const adventurer = {
     }
 }
 adventurer.roll();
+
+class Character {
+    constructor (name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+}
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
